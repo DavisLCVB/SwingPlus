37f4b2262907a576a59681bde77db9c4a533aa89
Davis <daviscvb22092004@gmail.com>
2024-01-10 00:00:24 -0500

    Correción documentación de RoundGeneration

--- src/utilities/RoundGeneration.java
+++ src/utilities/RoundGeneration.java
@@ -6,8 +6,31 @@ import java.awt.*;
  * <code>RoundGeneration</code> is an interface that contains the methods to create the corners of a rounded shape.
  */
 public interface RoundGeneration {
+    /**
+     * Creates a new <code>Shape</code> with the specified corner radius.
+     *
+     * @return a new <code>Shape</code> with the specified corner radius
+     */
     Shape createCornerTL();
+
+    /**
+     * Creates a new <code>Shape</code> with the specified corner radius.
+     *
+     * @return a new <code>Shape</code> with the specified corner radius
+     */
     Shape createCornerTR();
+
+    /**
+     * Creates a new <code>Shape</code> with the specified corner radius.
+     *
+     * @return a new <code>Shape</code> with the specified corner radius
+     */
     Shape createCornerBL();
+
+    /**
+     * Creates a new <code>Shape</code> with the specified corner radius.
+     *
+     * @return a new <code>Shape</code> with the specified corner radius
+     */
     Shape createCornerBR();
 }

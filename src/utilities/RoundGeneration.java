362d58b0f47f4ecab35fd29461c4b408dc46a005
Davis <daviscvb22092004@gmail.com>
2024-01-09 23:57:16 -0500

    Generada documentación de las clases RoundGeneration y RoundInfo

--- src/utilities/RoundGeneration.java
+++ src/utilities/RoundGeneration.java
@@ -2,6 +2,9 @@ package utilities;
 
 import java.awt.*;
 
+/**
+ * <code>RoundGeneration</code> is an interface that contains the methods to create the corners of a rounded shape.
+ */
 public interface RoundGeneration {
     Shape createCornerTL();
     Shape createCornerTR();
--- src/utilities/RoundInfo.java
+++ src/utilities/RoundInfo.java
@@ -2,12 +2,38 @@ package utilities;
 
 import java.io.Serializable;
 
+/**
+ * <code>RoundInfo</code> is a class that contains the corner radius information.
+ */
 public class RoundInfo implements Serializable {
+    /**
+     * This field contains the corner radius of the top left corner.
+     */
     private int radiusTL;
+
+    /**
+     * This field contains the corner radius of the top right corner.
+     */
     private int radiusTR;
+
+    /**
+     * This field contains the corner radius of the bottom left corner.
+     */
     private int radiusBL;
+
+    /**
+     * This field contains the corner radius of the bottom right corner.
+     */
     private int radiusBR;
 
+    /**
+     * Creates a new <code>RoundInfo</code> with the specified corner radius information.
+     *
+     * @param radiusTL the corner radius of the top left corner
+     * @param radiusTR the corner radius of the top right corner
+     * @param radiusBL the corner radius of the bottom left corner
+     * @param radiusBR the corner radius of the bottom right corner
+     */
     public RoundInfo(int radiusTL, int radiusTR, int radiusBL, int radiusBR) {
         this.radiusTL = radiusTL;
         this.radiusTR = radiusTR;
@@ -15,6 +41,11 @@ public class RoundInfo implements Serializable {
         this.radiusBR = radiusBR;
     }
 
+    /**
+     * Creates a new <code>RoundInfo</code> with the specified corner radius information.
+     *
+     * @param radius the corner radius of all corners
+     */
     public RoundInfo(int radius) {
         this.radiusTL = radius;
         this.radiusTR = radius;
@@ -22,37 +53,81 @@ public class RoundInfo implements Serializable {
         this.radiusBR = radius;
     }
 
+    /**
+     * Creates a new <code>RoundInfo</code> with the default corner radius (<code>0</code>).
+     */
     public RoundInfo() {
+        this(0);
     }
 
+    /**
+     * Returns the corner radius of the top left corner.
+     *
+     * @return the corner radius of the top left corner
+     */
     public int getRadiusTL() {
         return radiusTL;
     }
 
+    /**
+     * Sets the corner radius of the top left corner.
+     *
+     * @param radiusTL the corner radius of the top left corner
+     */
     public void setRadiusTL(int radiusTL) {
         this.radiusTL = radiusTL;
     }
 
+    /**
+     * Returns the corner radius of the top right corner.
+     *
+     * @return the corner radius of the top right corner
+     */
     public int getRadiusTR() {
         return radiusTR;
     }
 
+    /**
+     * Sets the corner radius of the top right corner.
+     *
+     * @param radiusTR the corner radius of the top right corner
+     */
     public void setRadiusTR(int radiusTR) {
         this.radiusTR = radiusTR;
     }
 
+    /**
+     * Returns the corner radius of the bottom left corner.
+     *
+     * @return the corner radius of the bottom left corner
+     */
     public int getRadiusBL() {
         return radiusBL;
     }
 
+    /**
+     * Sets the corner radius of the bottom left corner.
+     *
+     * @param radiusBL the corner radius of the bottom left corner
+     */
     public void setRadiusBL(int radiusBL) {
         this.radiusBL = radiusBL;
     }
 
+    /**
+     * Returns the corner radius of the bottom right corner.
+     *
+     * @return the corner radius of the bottom right corner
+     */
     public int getRadiusBR() {
         return radiusBR;
     }
 
+    /**
+     * Sets the corner radius of the bottom right corner.
+     *
+     * @param radiusBR the corner radius of the bottom right corner
+     */
     public void setRadiusBR(int radiusBR) {
         this.radiusBR = radiusBR;
     }
